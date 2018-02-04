eb267aff0dcd910ab1d552a5f4fbfcfbdb7fa186
ebreton <email>
2018-02-04 21:23:18 +0100

    default APP_SECRET is a uuid

--- src/settings.py
+++ src/settings.py
@@ -4,7 +4,7 @@ import uuid
 from utils import get_optional_env, get_mandatory_env
 
 VERSION = "0.1.2"
-APP_SECRET = get_optional_env("APP_SECRET", uuid.uuid4())
+APP_SECRET = get_optional_env("APP_SECRET", str(uuid.uuid4()))
 
 DB_CLASS = get_optional_env("DB_CLASS", "models.Store")
 DB_URI = get_mandatory_env("DB_URI")
